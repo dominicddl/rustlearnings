062fc31c20fa8ee040b955b2804315d0452559e6
dominicddl <e1398293@u.nus.edu>
2026-05-04 15:19:30 +0800

    init rust learnings with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d
@@ -0,0 +1,5 @@
+/Users/dominiccabansay/rustlearnings/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d: src/main.rs
+
+/Users/dominiccabansay/rustlearnings/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/dominiccabansay/rustlearnings/hello_cargo/target/debug/hello_cargo: /Users/dominiccabansay/rustlearnings/hello_cargo/src/main.rs
